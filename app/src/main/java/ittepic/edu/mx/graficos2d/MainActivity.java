e39a20918d754cbfa095e25f884deababa607a81
irvingm <irvingmarin14@gmail.com>
2018-04-24 22:03:38 -0600

    Initial commit

---
+++ app/src/main/java/ittepic/edu/mx/graficos2d/MainActivity.java
@@ -0,0 +1,13 @@
+package ittepic.edu.mx.graficos2d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
